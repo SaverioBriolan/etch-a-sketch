dfa5250a84c976c12774a7971762a362e4e2cf07
SaverioBriolan <64378841+SaverioBriolan@users.noreply.github.com>
2023-06-22 15:29:39 +0200

    Choose number of cell

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,37 @@
 
 const container = document.querySelector('.container');
-function createCell (nb) {
-   for (i=0; i < nb; i++){
+function createCell (nbCell) {
+    cellSize = 640 / nbCell;
+    nbCell = nbCell * nbCell;
+    for (i=0; i < nbCell; i++){
         const cell = document.createElement('div');
         container.appendChild(cell);
         cell.className = 'cell';
+         cell.setAttribute('style', "width: "+cellSize+"px; height: "+cellSize+"px;");
     }
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseenter", (event) => {
+            event.target.style.backgroundColor = "#403d39";
+        })
+    });
 }
-createCell(256);
-const cells = document.querySelectorAll('.cell');
+
+function removeCell() {
+    const cells = document.querySelectorAll('.cell');
 cells.forEach((cell) => {
-    cell.addEventListener("mouseenter", (event) => {
-        event.target.style.backgroundColor = "#403d39";
-    })
+        container.removeChild(cell)
 });
+}
+
+createCell(16);
+
+function askNumber(){
+    let nbCell= prompt('Hi ! How many squares for one line do you want ? (max: 100)');
+    removeCell();
+    createCell(nbCell);
+}
+
+
 
 
